4aedea13d7ee84c49ca85eebe1ec02fc14c1f04c
rylan <rylan.majoor@dcmail.ca>
2026-04-10 12:37:34 -0400

    Update main.js
    more changes

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -2,9 +2,6 @@
 
 const customName = document.getElementById("custom-name");
 const generateBtn = document.querySelector(".generate");
-const randomCharacter = randomValueFromArray(characters);
-const randomPlace = randomValueFromArray(places);
-const randomEvent = randomValueFromArray(events);
 const story = document.querySelector(".story");
 
 function randomValueFromArray(array) {
@@ -30,8 +27,10 @@ const events = [
 ];
 
 function returnRandomStoryString() {
-    // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-
+    const randomCharacter = randomValueFromArray(characters);
+    const randomPlace = randomValueFromArray(places);
+    const randomEvent = randomValueFromArray(events);
+    const storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
     return storyText;
 }
 
@@ -40,8 +39,10 @@ function returnRandomStoryString() {
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+    let newStory = returnRandomStoryString();
     if (customName.value !== "") {
         const name = customName.value;
+        newStory = newStory.replaceAll("Bob", name);
     }
 
     if (document.getElementById("uk").checked) {
