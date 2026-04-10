5b9fdcdc756862088336853c012378c99a3d7572
rylan <rylan.majoor@dcmail.ca>
2026-04-10 12:30:20 -0400

    starting
    declaring the consts, and raw text into arrays

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -2,6 +2,9 @@
 
 const customName = document.getElementById("custom-name");
 const generateBtn = document.querySelector(".generate");
+const randomCharacter = randomValueFromArray(characters);
+const randomPlace = randomValueFromArray(places);
+const randomEvent = randomValueFromArray(events);
 const story = document.querySelector(".story");
 
 function randomValueFromArray(array) {
@@ -10,20 +13,21 @@ function randomValueFromArray(array) {
 }
 
 // Raw text strings
-
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
-
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
-
-// Partial return random string function
+const characters = [
+    "Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas"
+];
+const places = [
+    "the soup kitchen",
+    "Disneyland",
+    "the White House"
+];
+const events = [
+    "spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+    "turned into a slug and slithered away"
+];
 
 function returnRandomStoryString() {
     // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
